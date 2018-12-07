4eb616880d30ea3a123ac1325513a8341c144929
pickJasmin <508437281@qq.com>
2018-12-07 08:10:14 +0800

    enen

---
+++ (20181007)Test01/WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello!MVC!";
+        }
+        public Customer getCustomer()
+        {
+            Customer ct = new Customer();
+            ct.CustomerName = "hao";
+            ct.Address= "rnrnr";
+            return ct;
+        }
+
+    }
+}
