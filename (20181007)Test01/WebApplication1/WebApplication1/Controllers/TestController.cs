07e5523edaaaace90090ad1290ad7bc3be7cad3a
PickJasmin <508437281@qq.com>
2018-12-07 12:09:18 +0800

    enene

--- (20181007)Test01/WebApplication1/WebApplication1/Controllers/TestController.cs
+++ (20181007)Test01/WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -17,8 +17,40 @@ namespace WebApplication1.Controllers
         {
             Customer ct = new Customer();
             ct.CustomerName = "hao";
-            ct.Address= "rnrnr";
+            ct.Address = "rnrnr";
             return ct;
+        }
+
+        public ActionResult GetView()
+        {
+
+            string greeting;
+            //获取当前时间
+            DateTime dt = DateTime.Now;
+            //获取当前小时数
+            int h = dt.Hour;
+            
+            //根据小时数判断需要返回哪个视图，小于12返回myview,大于返回yourview
+            if (h < 12)
+            {
+                greeting = "早上好";
+
+            }
+            else
+            {
+                greeting = "下午好";
+            }
+            //获取或设置视图数据的字典
+            ViewData["greeting"] = greeting;
+
+            Employee emp = new Employee();
+            emp.Name = "潘水荣";
+            emp.Salary = 5;
+            ViewData["EmpKey"] = emp;
+            return View("MyView");
+
+
+
         }
 
     }
