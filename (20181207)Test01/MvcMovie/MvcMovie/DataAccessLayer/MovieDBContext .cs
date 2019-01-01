d6138d15a724aacf5173b44499a08b2348aafdf9
pickJasmin <508437281@qq.com>
2019-01-01 22:27:15 +0800

    ene

---
+++ (20181207)Test01/MvcMovie/MvcMovie/DataAccessLayer/MovieDBContext .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MvcMovie.Models;
+
+namespace MvcMovie.DataAccessLayer
+{
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ (20181207)Test01/MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        //标题
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        //发布日期
+        public DateTime ReleaseDate { get; set; }
+        //类型电影”(Genre Film),
+        public string Genre { get; set; }
+        //价格；价值；代价
+        public decimal Price { get; set; }
+    }
+}
